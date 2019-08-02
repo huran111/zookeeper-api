2c6770695c14eb4619d10fec565fcbd2710d31b6
BUCHU <710625937@qq.com>
2019-08-02 18:57:18 +0800

    获取所有节点

--- src/main/java/com/hr/zk/CreateSession.java
+++ src/main/java/com/hr/zk/CreateSession.java
@@ -2,8 +2,10 @@ package com.hr.zk;
 
 import org.apache.zookeeper.*;
 import org.apache.zookeeper.data.Stat;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -16,7 +18,20 @@ import java.util.concurrent.CountDownLatch;
 public class CreateSession {
     private static final String CONNECTION_STRING = "192.168.10.179:2181";
     private static CountDownLatch countDownLatch = new CountDownLatch(1);
-
+    @Test
+    public  void list() throws IOException, InterruptedException, KeeperException {
+        ZooKeeper zooKeeper = new ZooKeeper(CONNECTION_STRING, 5000, new Watcher() {
+            @Override
+            public void process(WatchedEvent watchedEvent) {
+                if (watchedEvent.getState() == Event.KeeperState.SyncConnected) {
+                    countDownLatch.countDown();
+                }
+            }
+        });
+        countDownLatch.await();
+        List<String> children = zooKeeper.getChildren("/", true);
+        System.out.println(children.toString());
+    }
     public static void main(String[] args) throws IOException, InterruptedException {
         ZooKeeper zooKeeper = new ZooKeeper(CONNECTION_STRING, 5000, new Watcher() {
             @Override
@@ -39,6 +54,12 @@ public class CreateSession {
             String s1 = new String(data);
             System.out.println(s1);
             System.out.println(stat);
+            //修改数据
+            Stat stat1 = zooKeeper.setData("/liudehua", "bbbb".getBytes(), -1);
+            System.out.println(stat1);
+            //删除节点
+            zooKeeper.delete("/liudehua",-1);
+
         } catch (KeeperException e) {
             e.printStackTrace();
         }
